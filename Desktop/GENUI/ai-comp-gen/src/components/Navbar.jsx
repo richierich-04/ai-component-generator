aaf808af6fe46ac6a47f6bfdd51fd5fd5b27db4c
Richita Patnaik <richierich-04@users.noreply.github.com>
2025-09-10 23:46:29 +0530

    Setup project

---
+++ Desktop/GENUI/ai-comp-gen/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { IoIosSunny } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import {RiSettings3Fill} from "react-icons/ri";
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='nav flex items-center justify-between px-[100px] h-[90px] border-b-[1px] border-gray-500'>
+            <div className="logo">
+                <h3 className='text-[25px] font-[700] sp-text'>GenUI</h3>
+            </div>
+            <div className="icons flex items-center gap-[15px]">
+                <div className="icon"><IoIosSunny /></div>
+                <div className="icon"><FaUser /></div>
+                <div className="icon"><RiSettings3Fill /></div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
+
